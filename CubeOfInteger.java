d8ed4c4ff006aead902b41c84e790ad1fbe69e14
11florin <albuflorin929@yahoo.co.uk>
2025-03-26 21:34:57 +0000

    Add files via upload

---
+++ CubeOfInteger.java
@@ -0,0 +1,10 @@
+class CubeOfInteger
+{
+    public static void main(String[]args)
+    {
+        int x = 6;
+        int cubex = x*x*x;
+
+        System.out.println("The cube of "+x +" is " +cubex);
+    }
+}
